a5c9e3a4875b95614bd8ac31f9c50d55cfa52f56
Moffat <moffat.muga@gmail.com>
2023-03-24 17:50:51 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(_string) {
+  console.log(upperString);
+}
+  
+
+  
